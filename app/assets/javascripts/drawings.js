4e60456813901e8b5eebb118d476145a2634c44c
James Jacoby <james@builtbymoby.com>
2014-01-10 09:55:31 -0800

    solution

--- app/assets/javascripts/drawings.js
+++ app/assets/javascripts/drawings.js
@@ -1,7 +1,23 @@
 $(document).ready(function($) {
 	$.ajaxSetup({
-	  headers: {
-	    'X-CSRF-Token': $('meta[name="csrf-token"]').attr('content')
-	  }
+		headers: {
+			'X-CSRF-Token': $('meta[name="csrf-token"]').attr('content')
+		}
+	});
+
+	$('#draw_button').on('click', function(e) {
+		e.preventDefault();
+
+		$.ajax({
+			url: '/drawings',
+			type: 'POST',
+			dataType: 'json'
+		}).done(function(data) {
+			alert(data.name);
+			location.reload();
+		})
+		.fail(function() {
+			alert('unable to draw a winner');
+		});
 	});
 });
